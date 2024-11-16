396fc49bdefd67e9f0924e0162d32b3fe68eff38
synapticsurge <synapticsurge@proton.me>
2024-11-17 00:47:49 +0530

    removed println macro

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use tokio::io::{AsyncReadExt,AsyncWriteExt};
 
 #[tokio::main]
 async fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
     loop {
